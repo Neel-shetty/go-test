1bb32dbf37d19905af7348627570178d4cb5cca7
Neel <neelnarayanshetty@gmail.com>
2022-12-10 23:33:10 +0530

    tests

--- hello/hello.go
+++ hello/hello.go
@@ -3,10 +3,12 @@ package main
 import (
     "fmt"
     "neel/greetings"
+		"lib/test"
 )
 
 func main() {
     // Get a greeting message and print it.
     message := greetings.Hello("neel")
     fmt.Println(message)
+		num := Add(1)
 }
---
+++ hello/lib/test.go
@@ -0,0 +1,8 @@
+package test
+
+var added int32
+//adds num
+func Add(a int32) int32 {
+	added := (a+1)
+	return added
+}
